2c5e0402431b3177c897987ec2087aa49e864b6f
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-30 22:35:07 +0530

    update: updated the account for getting weth

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -1,5 +1,11 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network
+
+
 def get_weth():
-    pass
+    account = get_account()
+    weth = interface.IWeth(
+        config["networks"][network.show_active()]["weth_token"])
 
 
 def main():
