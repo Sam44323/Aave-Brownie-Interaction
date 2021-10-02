b112834f56893098ec334d41142bbb9cdb94fe39
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-02 14:05:57 +0530

    add: added scripts for aave deploy

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,5 +1,10 @@
 from scripts.get_weth import get_weth
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from brownie import config, network
 
 
 def main():
-    pass
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in [LOCAL_BLOCKCHAIN_ENVIRONMENTS, "kovan"]:
+        get_weth()
