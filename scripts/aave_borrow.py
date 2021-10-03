57f453e1d14b2b8055132b5c49b67f7fb0cfdb48
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-03 13:53:33 +0530

    add: added interface data fetcher

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -67,4 +67,4 @@ def get_borrowable_data(lending_pool, account):
 
 
 def get_asset_price(price_feed_address):
-    pass
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
